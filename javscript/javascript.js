93d301eb2107c666243291de3ba4d2ac1fa28427
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-03-08 15:39:52 +0100

    struttura else

--- javscript/javascript.js
+++ javscript/javascript.js
@@ -6,23 +6,23 @@ for (let i=1; i <= 100; i++){
     if((i % 3 == 0)  &&  (i % 5 == 0)){
         document.getElementById('general').innerHTML += "fizzbuzz"
         console.log('fizzbuzz')
-
+    
     }
 
-    if(i % 3 == 0){
+    else if(i % 3 == 0){
         console.log('fizz')
         document.getElementById('general').innerHTML += "fizz"
     }
 
-    if(i % 5 == 0){
+    else if(i % 5 == 0){
         console.log('buzz')
         document.getElementById('general').innerHTML += "buzz"
     }
 
-    if(!(i % 3 == 0)  &&  !(i % 5 == 0)){
+    else(!(i % 3 == 0)  &&  !(i % 5 == 0))
         document.getElementById('general').innerHTML += i
         console.log(i)
-    }
+    
 
 }
 
