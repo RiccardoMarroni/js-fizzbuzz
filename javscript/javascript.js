d242d787c0dec74472f572182598833517e8e644
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-03-08 15:24:56 +0100

    stampa su doc

--- javscript/javascript.js
+++ javscript/javascript.js
@@ -1,20 +1,31 @@
-for (let i=1; i <= 101; i++){
+
+
+
+for (let i=1; i <= 100; i++){
 
     if((i % 3 == 0)  &&  (i % 5 == 0)){
+        document.getElementById('general').innerHTML += "fizzbuzz"
         console.log('fizzbuzz')
 
     }
 
     if(i % 3 == 0){
         console.log('fizz')
+        document.getElementById('general').innerHTML += "fizz"
     }
 
     if(i % 5 == 0){
         console.log('buzz')
+        document.getElementById('general').innerHTML += "buzz"
     }
 
     if(!(i % 3 == 0)  &&  !(i % 5 == 0)){
+        document.getElementById('general').innerHTML += i
         console.log(i)
     }
+
 }
+
+
+
 
